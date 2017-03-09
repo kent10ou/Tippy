df1c80fd82a5147c0a4093219de40a27af2faeae
kent10ou <kent10ou@gmail.com>
2017-03-09 01:52:02 -0800

    slider now working

--- Tippy/Tippy/ViewController.swift
+++ Tippy/Tippy/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var personsSlider: UISlider!
+    @IBOutlet weak var personsLabel: UILabel!
+    @IBOutlet weak var splitTotalLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,16 +46,28 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        let currentValue = Int(sender.value)
+        if currentValue == 1 {
+            personsLabel.text = "\(currentValue) person"
+        } else {
+            personsLabel.text = "\(currentValue) people"
+        }
+    }
+
     @IBAction func calculateTip(sender: AnyObject) {
         
         let tipPercentages = [0.15, 0.18, 0.20]
-        
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
+        let persons = Double(Int(personsSlider.value))
+        let splitTotal = total / persons
+            
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        splitTotalLabel.text = String(format: "$%.2f",  splitTotal)
         
         if billField !== "" {
                 billField.frame.size.height = CGFloat(145)
